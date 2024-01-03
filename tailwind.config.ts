c6077d00cfe7aef1f54f8f3000e08919e9fec6aa
JuscelinoMamede <juscemamede01@gmail.com>
2024-01-03 12:00:45 -0300

    :wrench: tailwind and colors config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "bg-primary-color": "#6b5135",
+        "pallet-bright-color1": "#f2c6c6",
+        "pallet-bright-color2": "#d2aaa4",
+        "pallet-medium-color3": "#b28e82",
+        "pallet-medium-color4": "#927260",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
