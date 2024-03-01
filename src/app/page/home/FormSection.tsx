8ab841491251195bbf819d84847abaf72ef42ef3
JuscelinoMamede <juscemamede01@gmail.com>
2024-03-01 13:23:39 -0300

    configuração do email no form

--- src/app/page/home/FormSection.tsx
+++ src/app/page/home/FormSection.tsx
@@ -1,20 +1,32 @@
 "use client";
 
 import { Button, Checkbox, Label, TextInput, Textarea } from "flowbite-react";
+import { useForm, ValidationError } from "@formspree/react";
 
 import React from "react";
 
 export const FormSection = () => {
+  const [state, handleSubmit] = useForm("meqyknde");
+  if (state.succeeded) {
+    alert("Thank you for your Message!");
+  }
   return (
     <>
-      <section id="contact" className="flex flex-col justify-center items-center py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 ">
+      <section
+        id="contact"
+        className="flex flex-col justify-center items-center py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 "
+      >
         <h3
           className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl "
           data-aos="flip-up"
         >
           Contact me
         </h3>
-        <form className=" w-[100%] sm:w-[50%] flex flex-col gap-4">
+        <form
+          onSubmit={handleSubmit}
+          method="POST"
+          className=" w-[100%] sm:w-[50%] flex flex-col gap-4"
+        >
           <div>
             <div className="mb-2 block text-start">
               <Label htmlFor="email1" value="Your email" />
@@ -22,8 +34,14 @@ export const FormSection = () => {
             <TextInput
               id="email1"
               type="email"
+              name="email"
               placeholder="name@flowbite.com"
               required
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
             />
           </div>
 
@@ -34,8 +52,14 @@ export const FormSection = () => {
             <Textarea
               id="comment"
               placeholder="Leave a comment..."
+              name="comment"
               required
               rows={4}
+            />
+            <ValidationError
+              prefix="Comment"
+              field="comment"
+              errors={state.errors}
             />
           </div>
           <Button className="bg-gray-900 w-[50%]" type="submit">
